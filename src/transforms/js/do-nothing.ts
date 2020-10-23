c2bb33b6a3839f9ff8eade6fabe48c2227665dcd
Pakho Leung <pakholeung37@gmail.com>
2020-10-23 21:13:58 +0800

    setup

---
+++ src/transforms/js/do-nothing.ts
@@ -0,0 +1,7 @@
+import { Transform } from "jscodeshift"
+
+const transform: Transform = (fileInfo) => {
+  console.log(fileInfo)
+}
+
+export default transform
---
+++ tabs/tabs.js
@@ -0,0 +1,15 @@
+var Tab = {
+  _handleTabChange(e) {
+    var dataset = e.currentTarget.dataset;
+    var componentId = dataset.componentId;
+    var selectedId = dataset.itemId;
+    var data = { componentId, selectedId };
+
+    if (this.handleTabChange) {
+      this.handleTabChange(data);
+    } else {
+      console.warn('页面缺少 handleTabChange 回调函数');
+    }
+  }
+};
+module.exports = Tab;
