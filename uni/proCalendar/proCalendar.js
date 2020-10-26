dd8972ba591c97fdf3ffb90c72219ca793e3aedc
Pakho Leung <pakholeung37@gmail.com>
2020-10-26 23:24:12 +0800

    lint test

--- uni/proCalendar/proCalendar.js
+++ uni/proCalendar/proCalendar.js
@@ -1,19 +1,19 @@
 Component({
   options: {
-    addGlobalClass: true,
+    addGlobalClass: true
   },
   properties: {
     isOpenAmount: {
       type: Boolean,
-      value: false,
+      value: false
     },
     price: {
       type: String,
-      value: "",
+      value: ""
     },
     amount: {
       type: String,
-      value: "",
+      value: ""
     },
     timeMapData: {
       type: Object,
@@ -21,17 +21,17 @@ Component({
     },
     currencyVal: {
       type: String,
-      value:""
+      value: ""
     },
     multiSelect: {
       type: Boolean,
-      value: false,
+      value: false
     }
   },
   observers: {
-    "isOpenAmount, price, amount, timeMapData, currencyVal": function() {
-      this.clearData()
-      this.init()
+    "isOpenAmount, price, amount, timeMapData, currencyVal": function () {
+      this.clearData();
+      this.init();
     }
   },
   data: {
@@ -46,35 +46,32 @@ Component({
     selectStart: {
       year: "",
       month: "",
-      date: "",
+      date: ""
     },
     selectEnd: {
       year: "",
       month: "",
-      date: "",
+      date: ""
     },
-    cache: {},
+    cache: {}
   },
   methods: {
-    init() {  
+    init() {
       const now = new Date();
       const currentYear = this.data.currentYear || now.getFullYear();
       const currentMonth = this.data.currentMonth || now.getMonth() + 1;
-
       const nowYear = now.getFullYear();
       const nowMonth = now.getMonth() + 1;
-
       const dateArr = this.getDateArrByYearAndMonth(currentYear, currentMonth);
-
       this.setData({
         currentYear,
         currentMonth,
         nowYear,
         nowMonth,
-        dateArr,
+        dateArr
       });
-
     },
+
     clearData() {
       //取消选择
       const selectYear = "";
@@ -83,67 +80,66 @@ Component({
       const selectStart = {
         year: "",
         month: "",
-        date: "",
+        date: ""
       };
       const selectEnd = {
         year: "",
         month: "",
-        date: "",
+        date: ""
       };
-
       this.setData({
         selectYear,
         selectMonth,
         selectDate,
         selectStart,
-        selectEnd,
+        selectEnd
       });
     },
-    getDateArrByYearAndMonth(year, month) {
 
+    getDateArrByYearAndMonth(year, month) {
       // 如果已经有cache, 那就从cache中返回, 不再重新计算
-      if(this.data.cache[`${year}-${month}`]) {
-        return this.data.cache[`${year}-${month}`]
+      if (this.data.cache[`${year}-${month}`]) {
+        return this.data.cache[`${year}-${month}`];
       }
 
       const DATE_YEAR = new Date().getFullYear();
       const DATE_MONTH = new Date().getMonth() + 1;
       const DATE_DAY = new Date().getDate();
+      month -= 1; //全部时间的月份都是按0~11基准，显示月份才+1
 
-      month -= 1;
-
-      //全部时间的月份都是按0~11基准，显示月份才+1
       const dateArr = []; //需要遍历的日历数组数据
-      let nextYear = 0;
-      //没有+1方便后面计算当月总天数
-      let nextMonth = month + 1;
-      //目标月1号对应的星期
-      let startWeek = getWeek(year, nextMonth, 1);
-      //获取目标月有多少天
+
+      let nextYear = 0; //没有+1方便后面计算当月总天数
+
+      let nextMonth = month + 1; //目标月1号对应的星期
+
+      let startWeek = getWeek(year, nextMonth, 1); //获取目标月有多少天
+
       let dayNums = getTotalDayByMonth(year, nextMonth);
 
       if (month + 1 > 11) {
         nextYear = year + 1;
         dayNums = new Date(nextYear, nextMonth, 0).getDate();
       }
+
       for (let j = -startWeek + 1; j <= dayNums; j++) {
         let tempWeek = -1;
+
         if (j > 0) {
           tempWeek = getWeek(year, nextMonth, j);
         }
+
         let disabled = false;
         let isOutDate = false;
-        if (
-          (j < DATE_DAY && year == DATE_YEAR && month + 1 == DATE_MONTH) ||
-          (month + 1 < DATE_MONTH && year == DATE_YEAR) ||
-          year < DATE_YEAR
-        ) {
+
+        if (j < DATE_DAY && year == DATE_YEAR && month + 1 == DATE_MONTH || month + 1 < DATE_MONTH && year == DATE_YEAR || year < DATE_YEAR) {
           //当天之前的日期不可用
           disabled = true;
           isOutDate = true;
         }
 
         let price, amount;
+
         if (!disabled) {
           price = this.data.price;
           amount = this.data.amount;
@@ -154,13 +150,14 @@ Component({
           disabled: disabled,
           isOutDate: isOutDate,
           price: price,
-          amount: amount,
+          amount: amount
         });
       }
 
       if (this.data.timeMapData) {
         for (var i = 0; i < dateArr.length; i++) {
           var item = dateArr[i];
+
           if (item.dateNum <= 0) {
             continue;
           }
@@ -168,6 +165,7 @@ Component({
           var currentDate = new Date(year, month, item.dateNum);
           var currentDateStr = dateFormat("yyyy-MM-dd", currentDate);
           var targetObj = this.data.timeMapData[currentDateStr];
+
           if (targetObj) {
             if (item.isOutDate) {
               item.price = "";
@@ -190,29 +188,26 @@ Component({
 
       return dateArr;
     },
+
     handleClickDate(e) {
       const dataset = e.currentTarget.dataset || {};
       const dateNum = dataset.date || 0;
-      const disabled = dataset.disabled || 0;
-      // 选择不可选择单元或置灰单元, 不做任何操作
+      const disabled = dataset.disabled || 0; // 选择不可选择单元或置灰单元, 不做任何操作
+
       if (dateNum <= 0 || disabled) {
         return;
-      }
-      // 单选日期 
-      if(!this.data.multiSelect) {
+      } // 单选日期 
+
+
+      if (!this.data.multiSelect) {
         let currentYear = this.data.currentYear;
         let currentMonth = this.data.currentMonth;
-  
         let selectYear = this.data.selectYear;
         let selectMonth = this.data.selectMonth;
         let selectDate = this.data.selectDate;
-  
         let tmpDate = null;
-        if (
-          selectYear == currentYear &&
-          selectMonth == currentMonth &&
-          selectDate == dateNum
-        ) {
+
+        if (selectYear == currentYear && selectMonth == currentMonth && selectDate == dateNum) {
           //之前已经选中了这个日期，取消选择
           selectYear = "";
           selectMonth = "";
@@ -222,34 +217,30 @@ Component({
           selectYear = currentYear;
           selectMonth = currentMonth;
           selectDate = dateNum;
-  
-          tmpDate = new Date(
-            selectYear,
-            selectMonth - 1,
-            selectDate
-          );
+          tmpDate = new Date(selectYear, selectMonth - 1, selectDate);
         }
+
         this.setData({
           selectYear,
           selectMonth,
           selectDate
         });
-  
         let dateStr = null;
+
         if (tmpDate) {
           dateStr = dateFormat("yyyy-MM-dd", tmpDate);
         }
+
         this.triggerEvent("change", dateStr);
       } else {
         // 日期范围
         let selectStart = this.data.selectStart;
         let selectEnd = this.data.selectEnd;
-
         let selected = {
           year: this.data.currentYear,
           month: this.data.currentMonth,
           date: dateNum
-        }
+        };
         /**
          * 当selectStart无值时, 直接赋值给selectStart, selectEnd赋空值
          * 
@@ -257,36 +248,34 @@ Component({
          * 1. 当selectEnd有值时, 此时重置selectStart为新选择值, 重置selectEnd为空值
          * 2. 当selectEnd无值, 并且新选值大于selectStart时, 将selectEnd赋值为新选值
          */
-        console.log("click", selectEnd, selectStart, selected)
-        if (
-          !isADate(selectEnd) 
-          && isADate(selectStart) 
-          && greaterThan(selected, selectStart) 
-          /** 检查区间中是否有disabled */
-          && !this.isDateRangeHasDisabled(selectStart, selected)
-        ) {
+
+        console.log("click", selectEnd, selectStart, selected);
+
+        if (!isADate(selectEnd) && isADate(selectStart) && greaterThan(selected, selectStart)
+        /** 检查区间中是否有disabled */
+        && !this.isDateRangeHasDisabled(selectStart, selected)) {
           selectEnd = selected;
         } else {
           selectStart = selected;
           selectEnd = {
             year: "",
             month: "",
-            date: "",
-          }
+            date: ""
+          };
         }
-        console.log(selectStart, selectEnd)
-        this.triggerEvent("change", [selectStart, selectEnd])
+
+        console.log(selectStart, selectEnd);
+        this.triggerEvent("change", [selectStart, selectEnd]);
         this.setData({
           selectStart,
-          selectEnd,
-        })
+          selectEnd
+        });
       }
-
     },
+
     toLastMonth() {
       const DATE_YEAR = new Date().getFullYear();
       const DATE_MONTH = new Date().getMonth() + 1;
-
       let currentYear = this.data.currentYear;
       let currentMonth = this.data.currentMonth;
 
@@ -296,27 +285,26 @@ Component({
 
       const tmpDate = new Date(currentYear, currentMonth - 1);
       tmpDate.setMonth(tmpDate.getMonth() - 1);
-
       const nowDate = new Date(DATE_YEAR, DATE_MONTH - 1);
+
       if (tmpDate < nowDate) {
         this.setData({
           currentYear: DATE_YEAR,
-          currentMonth: DATE_MONTH,
+          currentMonth: DATE_MONTH
         });
         return;
       }
 
       this.setData({
         currentYear: tmpDate.getFullYear(),
-        currentMonth: tmpDate.getMonth() + 1,
+        currentMonth: tmpDate.getMonth() + 1
       });
-
       this.init();
     },
+
     toNextMonth() {
       const DATE_YEAR = new Date().getFullYear();
       const DATE_MONTH = new Date().getMonth() + 1;
-
       let currentYear = this.data.currentYear;
       let currentMonth = this.data.currentMonth;
 
@@ -326,30 +314,34 @@ Component({
 
       let tmpDate = new Date(currentYear, currentMonth - 1);
       tmpDate.setMonth(tmpDate.getMonth() + 1);
-
       this.setData({
         currentYear: tmpDate.getFullYear(),
-        currentMonth: tmpDate.getMonth() + 1,
+        currentMonth: tmpDate.getMonth() + 1
       });
       this.init();
     },
+
     isDateRangeHasDisabled(start, end) {
-      if(!this.data.timeMapData) return false
-      // 直接穷举出所有选中日期
+      if (!this.data.timeMapData) return false; // 直接穷举出所有选中日期
+
       const arr = [];
       const startTimeStamp = toDate(start).getTime();
       const endDateTimeStamp = toDate(end).getTime();
-      for(let timeStamp = startTimeStamp; timeStamp <= endDateTimeStamp; timeStamp += 86_400_000) {
-        arr.push(dateFormat("yyyy-MM-dd", new Date(timeStamp)))
-      }
-      // 判断日期是否在timeMapData中
-      return !arr.every(date => this.data.timeMapData[date] !== undefined)
+
+      for (let timeStamp = startTimeStamp; timeStamp <= endDateTimeStamp; timeStamp += 86_400_000) {
+        arr.push(dateFormat("yyyy-MM-dd", new Date(timeStamp)));
+      } // 判断日期是否在timeMapData中
+
+
+      return !arr.every(date => this.data.timeMapData[date] !== undefined);
     }
+
   },
   lifetimes: {
     attached() {
       this.init();
     }
+
   }
 });
 
@@ -366,30 +358,48 @@ function getWeek(year, month, date) {
 
 function dateFormat(fmt, date) {
   var o = {
-    "M+": date.getMonth() + 1,                 //月份
-    "d+": date.getDate(),                    //日
-    "h+": date.getHours(),                   //小时
-    "m+": date.getMinutes(),                 //分
-    "s+": date.getSeconds(),                 //秒
-    "q+": Math.floor((date.getMonth() + 3) / 3), //季度
-    "S": date.getMilliseconds()             //毫秒
+    "M+": date.getMonth() + 1,
+    //月份
+    "d+": date.getDate(),
+    //日
+    "h+": date.getHours(),
+    //小时
+    "m+": date.getMinutes(),
+    //分
+    "s+": date.getSeconds(),
+    //秒
+    "q+": Math.floor((date.getMonth() + 3) / 3),
+    //季度
+    "S": date.getMilliseconds() //毫秒
+
   };
+
   if (/(y+)/.test(fmt)) {
     fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
   }
+
   for (var k in o) {
     if (new RegExp("(" + k + ")").test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
     }
   }
+
   return fmt;
 }
 
-function toDate({year, month, date}) {
+function toDate({
+  year,
+  month,
+  date
+}) {
   return new Date(year, month - 1, date);
 }
 
-function isADate({year, month, date}) {
+function isADate({
+  year,
+  month,
+  date
+}) {
   return year && month && date;
 }
 
--- uni/productcategory/productcategory.js
+++ uni/productcategory/productcategory.js
@@ -1,21 +1,26 @@
 const app = getApp();
+
 var Fai = require("../../utils/util");
-var { Components } = require("../../components/components");
+
+var {
+  Components
+} = require("../../components/components");
+
 let COL_TYPE = 19;
 let isShowLoading = true;
 Page(Object.assign({}, Components, {
   data: {
     themeId: getApp().globalData.themeId || 1,
-    moduleList:[],
-    productSearchModule:{},
-    productCategoryModule:{},
-    categoryList:[],
-    showCategoryListPanel:false,
+    moduleList: [],
+    productSearchModule: {},
+    productCategoryModule: {},
+    categoryList: [],
+    showCategoryListPanel: false,
     colType: COL_TYPE,
     pageContentHeight: 0,
     pageWidth: 0,
-    selectIndex:0,
-    productCategoryModuleIndex:0,
+    selectIndex: 0,
+    productCategoryModuleIndex: 0,
     menuFixed: false,
     isIpx: getApp().globalData.isIpx || false
   },
@@ -26,16 +31,17 @@ Page(Object.assign({}, Components, {
         let pageWidth = window.windowWidth;
         let pageHeight = Fai.pxToRpx(window.windowHeight, pageWidth);
         let pageContentHeight = 0;
+
         if (getApp().globalData.isModel) {
-          pageContentHeight = pageHeight - 96;//不知道模板为什么固定底部高度96，这里没考虑单独访问的情况么。。。
+          pageContentHeight = pageHeight - 96; //不知道模板为什么固定底部高度96，这里没考虑单独访问的情况么。。。
         } else if (!that.data.isTabBarPath) {
-          pageContentHeight = pageHeight;//如果底部导航不存在，那么需要铺满全屏
+          pageContentHeight = pageHeight; //如果底部导航不存在，那么需要铺满全屏
         } else if (getApp().globalData.isIpx) {
-          pageContentHeight = pageHeight - 140;//iphoneX的情况下，底部高度是140rpx
+          pageContentHeight = pageHeight - 140; //iphoneX的情况下，底部高度是140rpx
         } else {
-          pageContentHeight = pageHeight - 96;//android情况下，底部导航是96rpx
+          pageContentHeight = pageHeight - 96; //android情况下，底部导航是96rpx
         }
-        
+
         that.setData({
           pageContentHeight: pageContentHeight,
           pageWidth: pageWidth
@@ -43,16 +49,17 @@ Page(Object.assign({}, Components, {
       }
     });
   },
-  onLoad: function(){
+  onLoad: function () {
     wx.removeStorageSync("mallStarToday");
-    new getApp().InitData(this);
-    // new getApp().FooterInit(this);
+    new getApp().InitData(this); // new getApp().FooterInit(this);
+
     new getApp().ProductCategoryInit(this);
     new getApp().PdFilterInit(this);
     Fai.setPageTitle(COL_TYPE);
     let that = this;
 
     that._editTabBar();
+
     if (that.data.dataCache != null) {
       let moduleList = that.data.dataCache;
       that.packagingPageData(moduleList);
@@ -72,64 +79,69 @@ Page(Object.assign({}, Components, {
       },
       success: res => {
         let data = res.data || {};
+
         if (data.success) {
           let moduleList = data.moduleList || [];
           that.packagingPageData(moduleList);
-        }else{
+        } else {
           Fai.logMonitor(6307, true);
         }
       }
     });
-  },  
+  },
   onShow: function () {
     getApp().ShowMallCartNum(this);
   },
-  packagingPageData: function(moduleList) {
+  packagingPageData: function (moduleList) {
     let productSearchModule = {},
-      productCategoryModuleIndex = 0,
-      productCategoryModule = {};
+        productCategoryModuleIndex = 0,
+        productCategoryModule = {};
 
     for (let i = 0; i < moduleList.length; i++) {
       let moduleData = moduleList[i];
-      if (moduleData.style == 6){
+
+      if (moduleData.style == 6) {
         moduleData.content.productList = this.sortProductList(moduleData.content.productList, moduleData.content.sn, moduleData.content.st);
-      }      
+      }
     }
 
     if (moduleList.length === 1) {
       productCategoryModule = moduleList[0] || {};
     } else {
-      for (let i = 0; i < moduleList.length;i++) {
-        if (moduleList[i].style === 6) {// 产品分类
+      for (let i = 0; i < moduleList.length; i++) {
+        if (moduleList[i].style === 6) {
+          // 产品分类
           productCategoryModule = moduleList[i];
           productCategoryModuleIndex = i;
         } else {
           productSearchModule = moduleList[i];
         }
       }
-      
     }
 
     if (!(Object.keys(productSearchModule).length === 0)) {
       this.setData({
         productSearchModule: productSearchModule,
         menuFixed: false
-      })
+      });
     }
 
     let categoryList = [];
-    if (productCategoryModule.content && Array.isArray(productCategoryModule.content.pcl)){
+
+    if (productCategoryModule.content && Array.isArray(productCategoryModule.content.pcl)) {
       categoryList = productCategoryModule.content.pcl;
     }
 
     this.setData({
       moduleList: moduleList,
-      productCategoryModuleIndex, productCategoryModuleIndex,
-      productCategoryModule, productCategoryModule,
+      productCategoryModuleIndex,
+      productCategoryModuleIndex,
+      productCategoryModule,
+      productCategoryModule,
       categoryList: categoryList,
       dataCache: moduleList
     });
-  },  
+  },
   showPanel: function () {
     let tmpSelectIndex = this.data.moduleList[this.data.productCategoryModuleIndex].selectIndex;
     this.setData({
@@ -148,21 +160,21 @@ Page(Object.assign({}, Components, {
       showCategoryListPanel: false
     });
   },
-  selectCategory:function(event){
+  selectCategory: function (event) {
     let groupId = event.currentTarget.dataset.groupid;
     let moduleIndex = event.currentTarget.dataset.moduleindex;
     let groupIndex = event.currentTarget.dataset.groupindex;
 
-    if (groupIndex == this.data.selectIndex ){
+    if (groupIndex == this.data.selectIndex) {
       return;
     }
-    this.onPclTabClick(event);
 
+    this.onPclTabClick(event);
     this.setData({
       showCategoryListPanel: false,
       selectIndex: groupIndex
     });
-  }, 
+  },
   toProductDetailPage: function (e) {
     let dataSet = e.currentTarget.dataset || {};
     let productInfo = dataSet.productinfo;
@@ -170,15 +182,12 @@ Page(Object.assign({}, Components, {
   },
   onPullDownRefresh: function () {
     let that = this;
-
     new getApp().InitData(this);
     new getApp().ProductCategoryInit(this);
     Fai.setPageTitle(COL_TYPE);
-
     wx.showLoading({
-      title: '加载中',
+      title: '加载中'
     });
-
     Fai.request({
       url: "wxmallapp_h.jsp?cmd=getModuleDataFromCol",
       data: {
@@ -192,6 +201,7 @@ Page(Object.assign({}, Components, {
       },
       success: res => {
         let data = res.data || {};
+
         if (data.success) {
           let moduleList = data.moduleList || [];
           that.packagingPageData(moduleList);
@@ -199,7 +209,7 @@ Page(Object.assign({}, Components, {
             "productGroupList": data.productGroupList
           });
           wx.stopPullDownRefresh();
-        }else{
+        } else {
           Fai.logMonitor(6307, true);
         }
       }
@@ -207,17 +217,19 @@ Page(Object.assign({}, Components, {
   },
   onPageScroll: function (e) {
     let menuFixed = false;
-    if(e.scrollTop > 0){
-      if (Object.keys(this.data.productSearchModule).length > 0){
+
+    if (e.scrollTop > 0) {
+      if (Object.keys(this.data.productSearchModule).length > 0) {
         // 65是搜索模块的高度, 这里不使用createSelectorQuery以尽量减少动画效果的延迟
-        if (this.data.menuFixed === (e.scrollTop > 65)){
+        if (this.data.menuFixed === e.scrollTop > 65) {
           return;
         }
-        menuFixed = (e.scrollTop > 65);
-      }else{
-        menuFixed = true
+
+        menuFixed = e.scrollTop > 65;
+      } else {
+        menuFixed = true;
       }
-    }else{
+    } else {
       if (this.data.showCategoryListPanel) {
         return;
       }
@@ -225,13 +237,12 @@ Page(Object.assign({}, Components, {
 
     this.setData({
       menuFixed: menuFixed
-    })
+    });
   },
   sortProductList: function (productList, type, order) {
-    if (productList.length < 2 || type == undefined)
-      return productList;
+    if (productList.length < 2 || type == undefined) return productList;
     let topList = [],
-      sortList = [];
+        sortList = [];
     productList.forEach(function (item, index) {
       if (item.top > 0) {
         topList.push(item);
@@ -239,19 +250,23 @@ Page(Object.assign({}, Components, {
         sortList.push(item);
       }
     });
+
     if (type == "name") {
       sortList = sortList.sort(function compareFunction(itemA, itemB) {
         let itemAName = itemA.name;
         let itemBName = itemB.name;
         let rt = 0;
+
         if (order == "asc") {
           rt = itemAName.localeCompare(itemBName);
         } else {
           rt = itemBName.localeCompare(itemAName);
         }
+
         if (rt != 0) {
           return rt;
         }
+
         if (order == "asc") {
           return itemA.addedTime - itemB.addedTime;
         } else {
@@ -263,14 +278,17 @@ Page(Object.assign({}, Components, {
         let itemAValue = itemA[type];
         let itemBValue = itemB[type];
         let rt = 0;
+
         if (order == "asc") {
           rt = itemAValue - itemBValue;
         } else {
           rt = itemBValue - itemAValue;
         }
+
         if (rt != 0) {
           return rt;
         }
+
         if (order == "asc") {
           return itemA.addedTime - itemB.addedTime;
         } else {
@@ -278,6 +296,7 @@ Page(Object.assign({}, Components, {
         }
       });
     }
+
     productList = topList.concat(sortList);
     return productList;
   }
